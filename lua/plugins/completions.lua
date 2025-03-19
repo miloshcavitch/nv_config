9ba99086cc9e075eeae76586c8b9689ec02dee49
Milo Arnerich <milo.arnerich@gmail.com>
2025-03-19 17:54:52 -0400

    adds bufferline, completions, terminal and whichkey

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,5 +1,10 @@
 return {
 	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	{
@@ -9,10 +14,13 @@ return {
 			"rafamadriz/friendly-snippets",
 		},
 	},
+
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 			require("luasnip.loaders.from_vscode").lazy_load()
 			cmp.setup({
 				snippet = {
